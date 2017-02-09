611b59fea357a69f9c6cef5f33391a16331e03ff
Komang Luce <KopiSusu@Komangs-MBP.fios-router.home>
2017-02-08 19:37:41 -0500

    fix it please

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -45,7 +45,7 @@ module.exports = {
     new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
     // optimizations
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
